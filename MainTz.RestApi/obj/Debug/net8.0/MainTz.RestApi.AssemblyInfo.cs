fcf4ccbcf5b4576290384c39037abf93a0e3c876
TrevorSomething675 <88676741+TrevorSomething675@users.noreply.github.com>
2023-11-24 17:44:39 +0300

    Небольшие правки во вьюшке для админа

--- MainTz.RestApi/obj/Debug/net8.0/MainTz.RestApi.AssemblyInfo.cs
+++ MainTz.RestApi/obj/Debug/net8.0/MainTz.RestApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MainTz.RestApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b50a33fd90015e75425fb40b89c587d0ffa3bfb5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+103b7e79ade13ea289a1b70a00c4b1d1a38568e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("MainTz.RestApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MainTz.RestApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
