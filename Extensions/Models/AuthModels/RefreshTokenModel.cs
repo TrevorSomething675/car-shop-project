69b697f8a98152916eff3d81240c15dfa7fb84da
IvanIvanovHome <soalone999@mail.ru>
2023-12-11 00:16:30 +0300

    c1

--- Extensions/Models/AuthModels/RefreshTokenModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Extensions.Models.AuthModels
-{
-    public class RefreshTokenModel
-    {
-        public string Role { get; set; }
-        public string RefreshToken { get; set; }
-    }
-}
