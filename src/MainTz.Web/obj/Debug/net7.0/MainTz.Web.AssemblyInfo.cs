c32d3c7dd9fe421afe5b4cbc3f8e0c93cd7f68e0
TrevorSomething675 <soalone999@mail.ru>
2024-01-18 10:04:38 +0300

    Новый гитигнор файл

--- src/MainTz.Web/obj/Debug/net7.0/MainTz.Web.AssemblyInfo.cs
+++ src/MainTz.Web/obj/Debug/net7.0/MainTz.Web.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MainTz.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5296bef34af9a8a85a26c15bbf9a0bbb4780a237")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcf53100e64a564468e021e640c3137326df833e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MainTz.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MainTz.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
